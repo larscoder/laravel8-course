795c0dead38f20abc57563e9cab88987b41c575f
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-19 20:30:00 -0500

    Agregamos visualziación del detalle del curso

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('curso/{course:slug}', [PageController::class, 'course'])->name('course');
 
-Route::get('/', function () {
-    return view('index');
-})->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
